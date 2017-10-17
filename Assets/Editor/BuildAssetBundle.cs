fdaa5fcbdee8fc5f9c8cff308ab5554890fdc1a0
cx <chixuchixu@hotmail.com>
2017-10-17 21:22:34 +0800

    init

---
+++ Assets/Editor/BuildAssetBundle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateAssetBundles
+{
+
+	[MenuItem ("Assets/Build AssetBundles Windows")]
+	static void BuildAllAssetBundlesWindows ()
+	{
+		BuildPipeline.BuildAssetBundles ("Assets/AssetBundlesWindows", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+	}
+
+	[MenuItem ("Assets/Build AssetBundles Android")]
+	static void BuildAllAssetBundlesAndroid ()
+	{
+		BuildPipeline.BuildAssetBundles ("Assets/AssetBundlesAndroid", BuildAssetBundleOptions.None, BuildTarget.Android);
+	}
+
+	[MenuItem ("Assets/Build AssetBundles IOS")]
+	static void BuildAllAssetBundlesIOS ()
+	{
+		BuildPipeline.BuildAssetBundles ("Assets/AssetBundlesIOS", BuildAssetBundleOptions.None, BuildTarget.iOS);
+	}
+
+}
+
